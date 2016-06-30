f89d0c4725400f6407f2b1ab14942a70fb752fdc
jepayne1138 <jepayne1138@gmail.com>
2016-06-30 14:47:14 -0500

    Add functions to save and load trained NN models persistantly

--- neural_network.py
+++ neural_network.py
@@ -12,6 +12,17 @@ def save_model(model, filename):
       1. Architecture file (JSON)
       2. Weights file (HDF5)
     """
+    with open('{}.json'.format(filename), 'w') as arch_file:
+        arch_file.write(model.to_json())
+    model.save_weights('{}.h5'.format(filename))
+
+
+def load_model(filename):
+    """Loads a model from architecture and weight files with filename"""
+    with open('{}.json'.format(filename), 'r') as arch_file:
+        model = keras.models.model_from_json(arch_file.read())
+    model.load_weights('{}.h5'.format(filename))
+    return model
 
 
 if __name__ == '__main__':
