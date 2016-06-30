7fa9c53d392fdd9deae846c90f475c33a66de04a
jepayne1138 <jepayne1138@gmail.com>
2016-06-30 14:34:04 -0500

    Create neural network managing module and updated requirements

---
+++ neural_network.py
@@ -0,0 +1,18 @@
+import keras
+
+
+def main():
+    pass
+
+
+def save_model(model, filename):
+    """Saves a model using the given filename as the base name
+
+    Creates two files:
+      1. Architecture file (JSON)
+      2. Weights file (HDF5)
+    """
+
+
+if __name__ == '__main__':
+    main()
